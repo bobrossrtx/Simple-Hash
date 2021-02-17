867c252067f8b938183acd17a05ebdebea9d1ced
bobrossrtx <73446766+bobrossrtx@users.noreply.github.com>
2021-02-17 16:54:11 +0000

    Added and fixed password checker

--- simple-hash/src/pages/home.js
+++ simple-hash/src/pages/home.js
@@ -5,13 +5,13 @@ export default function Home() {
     const [pass, setPass] = useState("");
 
     const handleValue = () => {
-        fetch(".    /assets/Simple-Hash/pass_lists/passwords.json")
-            .then(response => {response.json(); console.log(response.text())})
+        fetch("/pass_lists/passwords.json")
+            .then(response => response.json())
             .then(respJson => {
                 let searchPass = pass.split(" ");
-
+                
                 let passwords = (
-                    respJson.filter((string) => {
+                    respJson.passwords.filter((string) => {
                         let containsAtLeastOneWord = false;
                         searchPass.forEach((word) => {
                             if (string.value === "") {
@@ -23,14 +23,12 @@ export default function Home() {
                     })
                 );
 
-                if (passwords.length > 6) {
+                if (passwords.length <= 5) {
                     document.getElementById("cop-res").innerHTML = passwords
                 } else {
                     document.getElementById("cop-res").innerHTML = passwords.length
                 }
-            }).error(err => {
-                console.log(err)
-            });
+            })
     };
 
 
@@ -56,10 +54,10 @@ export default function Home() {
                 })} type="text" className="cop-search" />
                 <input type="submit" onClick={handleValue} id="cop-search-sub" value="Check" className="cop-sub button btn-g" />
                 </for>
-                <h4 class="cop-res"><span class="cop-res-l">Results</span><br/><span id="cop-res"></span></h4>
+                <h4 className="cop-res"><span className="cop-res-l">Results</span><br/><span id="cop-res"></span></h4>
                 <p className="cop-xpl">
                     <h3 className="cop-xpl-title">How To</h3>
-                    <p class="cop-xpl-desc">All you have to do is enter the password you want to check and it will scan through the dictionary to see if your password has been leaked in our list.</p>
+                    <p className="cop-xpl-desc">All you have to do is enter the password you want to check and it will scan through the dictionary to see if your password has been leaked in our list.</p>
                 </p>
             </div>
         </div>
